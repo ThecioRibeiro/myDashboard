44f7ad50293599da446bbd8efe69a43170c734da
ThecioRibeiro <55919924+ThecioRibeiro@users.noreply.github.com>
2019-10-24 21:53:00 -0300

    24-10-2019

--- assets/js/alunos.js
+++ assets/js/alunos.js
@@ -1,24 +1,65 @@
 
 var aluno = {};
 var listaAlunos = [];
+var totalAlunos = 0;
 
 function adicionarAluno(){
-    aluno.nome = $('#nomeAluno').val();
-    aluno.nota1 = parseInt($('#primeiroBi').val());
-    aluno.nota2 = parseInt($('#segundoBi').val());
-    aluno.nota3 = parseInt($('#terceiroBi').val());
-    aluno.nota4 = parseInt($('#quartoBi').val());
+
+    if ($('#nomeAluno').valid()){
+        aluno.nome = $('#nomeAluno').val();
+        $('#nomeAluno').removeClass('is-invalid');
+    }else{
+        $('#nomeAluno').addClass('is-invalid');
+    }
+
+
+    if ($('#primeiroBi').valid()){
+        aluno.nota1 =  parseInt($('#primeiroBi').val());
+        $('#primeiroBi').removeClass('is-invalid');
+    }else{
+        $('#primeiroBi').addClass('is-invalid');
+    }
+
+    if ($('#segundoBi').valid()){
+        aluno.nota2 = parseInt($('#segundoBi').val());
+        $('#segundoBi').removeClass('is-invalid');
+    }else{
+        $('#segundoBi').addClass('is-invalid');
+    }
+
+    if ($('#terceiroBi').valid()){
+        aluno.nota3 = parseInt($('#terceiroBi').val());
+        $('#terceiroBi').removeClass('is-invalid');
+    }else{
+        $('#terceiroBi').addClass('is-invalid');
+    }
+
+    if ($('#quartoBi').valid()){
+        aluno.nota4 = parseInt($('#quartoBi').val());
+        $('#quartoBi').removeClass('is-invalid');
+    }else{
+        $('#quartoBi').addClass('is-invalid');
+    }
+
     aluno.total = calcularTotal(aluno.nota1, aluno.nota2, aluno.nota3, aluno.nota4);
     aluno.media = calcularMedia(aluno.total);
     aluno.situacao = calcularSituacao(aluno.media);
 
-    var form = $('#formAdicionarAluno');
+    console.log($('#nomeAluno').valid());
+    console.log($('#formAdicionarAluno').validate());
 
-    console.log($(':form').valid());
-    console.log($(':form').validate());
 
-    adicionarLinhaTabela(aluno);
-    $(':input').val ('');
+
+    if ($('#nomeAluno').valid()
+        && $('#primeiroBi').valid()
+        && $('#segundoBi').valid()
+        && $('#terceiroBi').valid()
+        && $('#quartoBi').valid()){
+            adicionarLinhaTabela(aluno);
+        $(':input').val ('');
+    }
+
+    aluno = {};
 }
 
 function calcularTotal(nota1, nota2, nota3, nota4) {
@@ -39,6 +80,9 @@ function calcularSituacao(media) {
 }
 
 function adicionarLinhaTabela(objeto) {
+    totalAlunos = totalAlunos + 1;
+    
+
     var tabela = document.querySelector('table');
     var tr = tabela.insertRow(1);
     tr.innerHTML = '<td>'+objeto.nome+'</td>'+
@@ -48,5 +92,17 @@ function adicionarLinhaTabela(objeto) {
                     '<td>'+objeto.nota4+'</td>'+
                     '<td>'+objeto.total+'</td>'+
                     '<td>'+objeto.media+'</td>'+
-                    '<td>'+objeto.situacao+'</td>';
+                    '<td>'+objeto.situacao+'</td>'+
+                    "<td><button type='button' class='btn btn-danger btn-sm' onclick='deletarLinha(this)'><i class='fas fa-minus-circle'></i></button></td>";
+                    toastr.success('Aluno adicionado com sucesso!!!');
+}
+
+function deletarLinha(linha) {
+    totalAlunos = totalAlunos - 1;
+    
+
+    var i = linha.parentNode.parentNode.rowIndex;
+    document.querySelector('table').deleteRow(i);
+    toastr.info('Aluno removido!!!');
+    //document.getElementById('tableAlunos').deleteRow(index)
 }
